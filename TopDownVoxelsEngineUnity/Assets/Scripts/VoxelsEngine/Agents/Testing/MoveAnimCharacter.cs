94aba851c0cebb95cf656de31d5eafb259c08921
Samuel Bouchet <contact@samuel-bouchet.fr>
2023-07-26 22:06:51 +0200

    Fine tune move and anims

--- TopDownVoxelsEngineUnity/Assets/Scripts/VoxelsEngine/Agents/Testing/MoveAnimCharacter.cs
+++ TopDownVoxelsEngineUnity/Assets/Scripts/VoxelsEngine/Agents/Testing/MoveAnimCharacter.cs
@@ -1,23 +1,32 @@
 using System;
+using LoneStoneStudio.Tools;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine;
 using VoxelsEngine.Tools;
 
 namespace VoxelsEngine {
     public class MoveAnimCharacter : MonoBehaviour {
 
+        [ShowInInspector]
+        public float DeltaTime => Time.deltaTime;
+        public float FixedDeltaTime => Time.deltaTime;
+        
+        [ShowInInspector]
+        public Vector3 Vel => _vel;
+        
         [Required]
         public FaceController FaceController = null!;
-        
+
         public Animator Animator;
-        
+
         [RequiredInScene]
         public Transform CameraTransform = null!;
 
         public float VisualSnappingStrength = 0.3f;
-        
+
         public float CameraHeightOffset = 2.5f;
-        
+
         public float RotationSensivityX = 1.4f;
         public float RotationSensivityZ = 0.5f;
 
@@ -31,10 +40,10 @@ namespace VoxelsEngine {
 
         // Define zoom sensitivity
         public float ZoomSensitivity = 10f;
-        
+
         public float CameraZoomTiltStrength = 0.5f;
 
-        
+
         public float Speed = 5.0f;
 
         private Controls _controls = null!;
@@ -43,6 +52,7 @@ namespace VoxelsEngine {
 
         public float JumpForce = 0.2f;
         public float JumpChargeIntensity = 1f;
+        public float Gravity = 0.4f;
 
         private readonly Cooldown _jumpCooldown = new(0.5f);
         private Vector3 _position;
@@ -52,6 +62,8 @@ namespace VoxelsEngine {
         private float _currentAngleZ;
         private bool _isRotating;
         private static readonly int Velocity = Animator.StringToHash("Velocity");
+        private static readonly int Altitude = Animator.StringToHash("Altitude");
+        private Vector3 _vel;
 
         // Start is called before the first frame update
         void Awake() {
@@ -59,60 +71,65 @@ namespace VoxelsEngine {
             _cam = CameraTransform.GetComponent<Camera>();
             _position = transform.position;
             _originalOffset = CameraTransform.position - transform.position;
-            
         }
 
         protected void OnEnable() {
             _controls.Enable();
+            _vel = Vector3.zero;
         }
 
         public void OnDisable() {
             _controls.Disable();
         }
-
-        // Update is called once per frame
         void Update() {
+            HandleCamera();
             var movement = HandleMove();
-            Animator.SetFloat(Velocity, movement.magnitude);
+            Animator.SetFloat(Velocity, movement.x*movement.x + movement.z*movement.z);
+            Animator.SetFloat(Altitude, _position.y);
             FaceController.CurrentFace = movement.magnitude > 0.001f ? FaceController.FACES.Angry : FaceController.FACES.Smile_Blink;
+            transform.position = Vector3.Lerp(transform.position, _position, VisualSnappingStrength * 10 * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, _rotation, VisualSnappingStrength * 10 * Time.deltaTime);
         }
 
         private Vector3 HandleMove() {
             Vector2 moveInput = _controls.Gameplay.Move.ReadValue<Vector2>();
             Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-            Vector3 movement = CameraTransform.rotation * move;
-            movement.y = 0;
-            movement = movement.normalized * (Speed * Time.deltaTime);
+            Vector3 moveDirection = (CameraTransform.rotation * move).WithY(0).normalized;
 
             // If we have some input
-            if (move != Vector3.zero) {
+            if (moveDirection != Vector3.zero) {
                 // Create a quaternion (rotation) based on looking down the vector from the player to the camera.
-                Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
+                Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
 
                 // Smoothly interpolate between current rotation and target rotation
                 _rotation = targetRotation; //Quaternion.Slerp(transform.rotation, targetRotation, 5.0f * Time.deltaTime);
             }
 
-
+            if (_position.y > 0) {
+                _vel.y -= Gravity * Time.deltaTime;
+            } else {
+                _vel.y = 0;
+            }
+            
             if (_controls.Gameplay.Jump.WasPressedThisFrame() && _jumpCooldown.TryPerform() && _position.y <= 0) {
                 _jumpChargeStart = Time.time;
-                movement.y = JumpForce;
-            } else if (_controls.Gameplay.Jump.IsPressed()) {
-                var jumpCharge = Time.time - _jumpChargeStart;
-                movement.y += JumpChargeIntensity * Time.fixedDeltaTime * (1 - Mathf.Clamp01(jumpCharge * jumpCharge));
+                _vel.y = JumpForce;
+            }
+            else if (_controls.Gameplay.Jump.IsPressed()) {
+                var jumpCharge = (Time.time - _jumpChargeStart) * 2;
+                _vel.y += JumpChargeIntensity * Time.deltaTime * (1 - Mathf.Clamp01(jumpCharge));
             }
 
-            _position += movement;
-            transform.position = UnityEngine.Vector3.Lerp(transform.position, _position, VisualSnappingStrength * 50 * Time.deltaTime);
-            transform.rotation = Quaternion.Slerp(transform.rotation, _rotation, VisualSnappingStrength * 50 * Time.deltaTime);
-
-            if (_position.y <= 0) {
+            _position += moveDirection * (Speed * Time.deltaTime) + new Vector3(0, _vel.y * Time.deltaTime, 0);
+           
+            if (_position.y <= 0.001f) {
                 _position.y = 0;
             }
 
-            // Rotation
-            //if (_controls.Gameplay.RotateCamera.IsPressed()) {
-            // Zooming
+            return moveDirection;
+        }
+
+        private void HandleCamera() {
             float scroll = Input.GetAxis("Mouse ScrollWheel");
             ZoomLevel -= scroll * ZoomSensitivity;
             // You might want to limit the zoom level to some minimum and maximum values
@@ -130,8 +147,6 @@ namespace VoxelsEngine {
             var offset = Vector3.up * 2.5f;
             CameraTransform.position = transform.position + _originalOffset + offset;
             CameraTransform.LookAt(transform.position + offset);
-
-            return movement;
         }
     }
 }
